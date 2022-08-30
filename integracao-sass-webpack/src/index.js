cc6e5f5dbb1d637e63d49ab8bf8ebeb547a63c7c
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-30 00:47:05 -0300

    Criação de novos diretórios, atualização do arquivo de configuração webpack

--- integracao-sass-webpack/src/index.js
+++ integracao-sass-webpack/src/index.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 import './sass/style.scss';
 import Button from './componentes/button/button.js';
+import Wallpaper from './images/tag_wallpaper.png';
 
 $(function () {
   let button = new Button();
-  button.setTitle('Teste');
+  button.setTitle('Mostrar Wallpaper');
   button.setClick(function () {
-    alert('Clicou');
+    alert('Clique em OK');
   });
   $('.area').html(button.render());
 });
